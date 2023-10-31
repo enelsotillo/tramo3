ac25353b8d1278c83e0ce137f2ce2cc18ebb2217
Enel Sotillo <enelsotillo@gmail.com>
2023-10-31 18:32:13 -0300

    creando servidor express

---
+++ src/controllers/usuarioModel.js
@@ -0,0 +1,21 @@
+const usuariosController ={}
+
+const lista_Usuario=[
+ {nombre:"Enel"},
+ {apellido: "Sotillo"}
+]
+
+
+//ver usurios
+usuariosController.verUsuario = (req, res) => {
+    return lista_Usuario;
+
+}
+//ver ususrio
+
+//crear usuario
+
+//actualizar usuario
+
+//eliminar usuario
+
